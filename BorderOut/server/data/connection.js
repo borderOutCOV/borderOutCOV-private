e0af15a30a904112205c7dcedaf1bc10421b9938
19AlexOrtega97 <ortega4414@gmail.com>
2019-02-05 10:54:49 -0600

    5/02/19

--- BorderOut/server/data/connection.js
+++
@@ -1,9 +0,0 @@
-const mysql = require('mysql');
-var connection = mysql.createConnection({
-    host: 'localhost',
-    user: 'root',
-    password: '',
-    database: 'limitbreaker'
-});
-
-module.exports = connection;
